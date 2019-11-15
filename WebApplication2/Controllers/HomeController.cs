bf19ad20b8efe0688c8012773e0528a59b441489
Maxim Shakurov <m.shakurov99@mail.ru>
2019-11-15 04:16:52 +0400

    init

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using WebApplication2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        DolznostContext db = new DolznostContext();
+        public ActionResult Index()
+        {
+            var sotrudniks = db.Sotrudnik.Include(p => p.Dolzhnost);
+            return View(sotrudniks.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/DolznostContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication2.Models
+{
+    public class DolznostContext : DbContext
+    {
+        public DbSet<Dolzhnost> Dolzhnost { get; set; }
+        public DbSet<Sotrudnik> Sotrudnik { get; set; }
+    }
+}
