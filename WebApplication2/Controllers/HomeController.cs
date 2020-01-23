25e761673699b81a7fce75a14516be61b29427cc
Maxim <m.shakurov99@mail.ru>
2020-01-23 18:56:32 +0400

    Конечные фиксы

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -12,73 +12,27 @@ namespace WebApplication2.Controllers
         public ActionResult Index()
         {
 
-            //using (var db = new db_hotelEntities())
-            //{
-            //    var sotrudniki = db.tSotrudnik.Include("tDolzhnost").ToList();
-
-            //    return View(sotrudniki);
-            //}
             return View();
         }
 
-        [HttpGet]
-        public ActionResult Create()
+        public ActionResult Registration()
         {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Create(tSotrudnik model)
-        {
-            using (var db = new db_hotelEntities())
-            {
-                db.tSotrudnik.Add(model);
-                db.SaveChanges();
-            }
-
-                return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        public ActionResult Delete(int? ID_Rabotnika)
-        {
-            if (ID_Rabotnika == 0)
-            {
-                ViewBag.Message = "ERROR";
-            }
-
-            using (var db = new db_hotelEntities())
-            {
-                var sotrudnik = db.tSotrudnik.Find(ID_Rabotnika);
-                return View(sotrudnik);
-            }
-        }
-
-        [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            using (var db = new db_hotelEntities())
-            {
-                tSotrudnik model = db.tSotrudnik.Find(id);
-                db.tSotrudnik.Remove(model);
-                db.SaveChanges();
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Login";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult ClickOnRegistration(string Password)
         {
-            ViewBag.Message = "Your contact page.";
+            if (Password == "admin")
+            {
 
-            return View();
+                return View();
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
